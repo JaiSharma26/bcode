2eea967bff217094c3cd685a10d37d9a9a02867d
JaiSharma26 <jai.jpsharma@yahoo.in>
2017-04-12 19:33:28 +0530

    bCode third

---
+++ application/views/frontend/index.php
@@ -0,0 +1,24 @@
+<div class="container" style="max-width:400px;">
+	<div class="form-group">
+		<label>Username /Email </label>
+		<input type="text" class="form-control" name="username_email">
+	</div>
+	<div class="form-group">
+		<label>Password </label>
+		<input type="text" class="form-control" name="password">
+	</div>
+
+	<div class="form-group row">
+
+		<span class="pull-left"><input type="checkbox" name="rememberme" value="rmemberme">Remember Me</span>
+		<span class="pull-right"><a href="#">Forget Password</a></span>
+
+	</div>
+
+	<div class="form-group">
+		<button class="btn btn-primary pull-left" type="submit">Login</button>
+		<span class="pull-right"><a href="#">Register</a></span>
+	</div>
+
+
+</div>
